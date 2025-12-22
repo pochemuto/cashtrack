3d6deb1cdc0bf7b20fcfae6fc21705bac02f84cf
Alexander Kramarev <pochemuto@yandex-team.ru>
2025-12-22 16:11:46 +0100

    Icons

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -96,7 +96,10 @@
                         <li class="item" class:removing={item.removing}>
                             <button class="btn btn-xs btn-secondary btn-ghost btn-circle"
                                     class:btn-disabled={item.removing}
-                                    onclick={() => remove(item)}>x
+                                    onclick={() => remove(item)}>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-3">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                                </svg>
                             </button>
                             {item.title}
                         </li>
@@ -120,7 +123,9 @@
                             {#if opLoading}
                                 <span class="loading loading-spinner"></span>
                             {:else}
-                                +
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                                </svg>
                             {/if}
                         </button>
                     </div>
